c69e1d1c3f08126f12a567463634ae6262bed2f6
Surenguna <g.surendran007@gmail.com>
2022-05-10 23:23:29 +0530

    Next code

---
+++ src/org/git/SampleGit.java
@@ -0,0 +1,25 @@
+package org.git;
+
+public class SampleGit {
+	
+	
+		
+		
+		public static void main(String[] args) {
+			
+			int a= 10;
+			int b= 20;
+			
+			int add = a+b;
+			int mul= a*b;
+			int div= b/a;
+			
+			System.out.println("The answer is:"+add);
+			System.out.println("The answer is :"+mul);
+			System.out.println("The answer is:"+div);
+		}
+
+	}
+
+
+
